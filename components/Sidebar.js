a969f8f4aa554b3aa637d4519b6c01e46821e01d
Fadhil Radhian <fadhilradhian@gmail.com>
2021-06-18 10:52:30 +0700

    sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,5 +1,28 @@
+import SidebarRow from "./SidebarRow";
+import {
+  ChevronDownIcon,
+  ShoppingBagIcon,
+  UserGroupIcon,
+} from "@heroicons/react/outline";
+import {
+  CalendarIcon,
+  ClockIcon,
+  DesktopComputerIcon,
+  UsersIcon,
+} from "@heroicons/react/solid";
+
 function Sidebar() {
-  return <div></div>;
+  return (
+    <div className="p-2 md:w-1/4 max-w-[600px] xl:min-w-[300px]">
+      <SidebarRow Icon={UsersIcon} title="Friends" />
+      <SidebarRow Icon={UserGroupIcon} title="Groups" />
+      <SidebarRow Icon={ShoppingBagIcon} title="Marketplace" />
+      <SidebarRow Icon={DesktopComputerIcon} title="Watch" />
+      <SidebarRow Icon={CalendarIcon} title="Events" />
+      <SidebarRow Icon={ClockIcon} title="Memories" />
+      <SidebarRow Icon={ChevronDownIcon} title="See More" />
+    </div>
+  );
 }
 
 export default Sidebar;
